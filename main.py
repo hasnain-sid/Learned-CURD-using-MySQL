535a8cc9f0d23eb7d36d40a478527d52d6210b65
Hasnain <hasnaincodeforces@gmail.com>
2023-05-22 20:05:16 +0530

    MqSQL with python

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import mysql.connector as connector
 
 class DBHelper:
     def __init__(self):
-        self.con = connector.connect(host='localhost',port = '3306',user = 'root',password = 'HasnainSid@786',database = 'pythontest')
+        self.con = connector.connect(host='localhost',port = '3306',user = 'root',password = 'root',database = 'pythontest')
 
         query = 'create table if not exists user(userID int primary key,userName varchar(200), phone varchar(12))'
         cur = self.con.cursor()
